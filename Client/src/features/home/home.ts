a1cc468d281cbe95cd26bdcebc0b9df9a4ef0eb6
eranseg74 <sgl.eran@gmail.com>
2025-11-20 18:37:10 +0200

    Updated home component

--- Client/src/features/home/home.ts
+++ Client/src/features/home/home.ts
@@ -1,6 +1,7 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
 import { Register } from '../account/register/register';
 import { User } from '../../types/user';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { User } from '../../types/user';
 export class Home {
   // @Input({ required: true }) membersFromApp: User[] = []; // Setting the required to true means that the template that conatins the app-home element must provide a list of users. Otherwise we would have to check here that we got something or else the array will always be empty
   protected registerMode = signal(false);
+  protected accountService = inject(AccountService);
 
   showRegister(value: boolean) {
     this.registerMode.set(value);
